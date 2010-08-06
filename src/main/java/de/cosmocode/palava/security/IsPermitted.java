511fb7ebf01ed5e43a2340f4f11e5fbe9e2a6263
schoenborn <schoenborn@a41a61bc-0f84-421d-9ab3-fe1244b22163>
2010-08-06 07:22:20 +0000

    organized imports

--- src/main/java/de/cosmocode/palava/security/IsPermitted.java
+++ src/main/java/de/cosmocode/palava/security/IsPermitted.java
@@ -30,10 +30,10 @@ import com.google.inject.Singleton;
 import de.cosmocode.palava.ipc.IpcArguments;
 import de.cosmocode.palava.ipc.IpcCall;
 import de.cosmocode.palava.ipc.IpcCommand;
-import de.cosmocode.palava.ipc.IpcCommandExecutionException;
 import de.cosmocode.palava.ipc.IpcCommand.Description;
 import de.cosmocode.palava.ipc.IpcCommand.Param;
 import de.cosmocode.palava.ipc.IpcCommand.Return;
+import de.cosmocode.palava.ipc.IpcCommandExecutionException;
 
 /**
  * See below.
--- src/main/java/de/cosmocode/palava/security/Logout.java
+++ src/main/java/de/cosmocode/palava/security/Logout.java
@@ -29,8 +29,8 @@ import com.google.inject.Singleton;
 
 import de.cosmocode.palava.ipc.IpcCall;
 import de.cosmocode.palava.ipc.IpcCommand;
-import de.cosmocode.palava.ipc.IpcCommandExecutionException;
 import de.cosmocode.palava.ipc.IpcCommand.Description;
+import de.cosmocode.palava.ipc.IpcCommandExecutionException;
 
 /**
  * See below.
